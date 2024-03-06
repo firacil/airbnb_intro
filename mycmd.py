30effb5dc0052641c0da91592564cb6507367225
firacil <firaolmekbib511@gmail.com>
2024-03-06 03:21:31 -0800

    updated

--- mycmd.py
+++ mycmd.py
@@ -5,13 +5,20 @@ class MyCmd(cmd.Cmd):
     """ making use of cmd"""
 
     prompt = "(hbnb) "
-
-    def do_echo(self, args):
-        """ handling the echo command"""
-        print(args)
+    
+    def do_EOF(self, arg):
+        """ handling the EOF command"""
+        print()
+        return True
 
     def do_quit(self, args):
+        """ handling the quit command"""
         return True
+    
+    def emptyline(self):
+        """Doesn't do anything on Enter
+        """
+        pass
 
 if __name__ == '__main__':
     MyCmd().cmdloop()
