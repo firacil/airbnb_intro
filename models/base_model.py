0b5ffc4042b31dd22abe4397239c0e7c82ed0ec1
firacil <firaolmekbib511@gmail.com>
2024-03-07 07:20:56 -0800

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class BaseModel:
     """ class defines all common attr/methods"""
     
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
